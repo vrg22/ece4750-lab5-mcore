6fe9bb1b67eee0abd71184a6d0d1e60c5156c574
abn44 <abn44@cornell.edu>
2014-09-09 14:06:11 -0400

    Test harness v.2

--- lab1-imul/lab1-imul-test-harness.v
+++ lab1-imul/lab1-imul-test-harness.v
@@ -191,6 +191,200 @@ module top;
   // make the above test case larger. Once you have finished adding
   // directed tests, move on to adding random tests.
 
+
+//----------------------------------------------------------------------
+  // Test Case: products of zero, one, and negative one
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 2, "products of zero, one, and negative one" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'd00, 32'd01, 32'd00  );
+    init( 1, 32'd01, 32'd00, 32'd00  );
+    init( 2, 32'd00, -32'd01, 32'd00 );
+    init( 3, -32'd01, 32'd00, 32'd00 );
+    init( 4, 32'd01, -32'd01, -32'd01);
+    init( 5, -32'd01, 32'd01, -32'd01);
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+//----------------------------------------------------------------------
+  // Test Case: small positive * negative
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 3, "small positive * negative" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'd02, -32'd03, -32'd06  );
+    init( 1, 32'd06, -32'd012, -32'd72 );
+    init( 2, 32'd08, -32'd04, -32'd32  );
+    init( 3, 32'd15, -32'd15, -32'd225 );
+    init( 4, 32'd01, -32'd02, -32'd02  );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: small negative * positive
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 4, "small negative * positive" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, -32'd02, 32'd03, -32'd06  );
+    init( 1, -32'd06, 32'd012, -32'd72 );
+    init( 2, -32'd08, 32'd04, -32'd32  );
+    init( 3, -32'd15, 32'd15, -32'd225 );
+    init( 4, -32'd01, 32'd01, -32'd01  );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: large positive * positive
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 5, "large positive * positive" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'hab, 32'hbc, 32'h7d94         );
+    init( 1, 32'h2c, 32'h88, 32'h1760         );
+    init( 2, 32'hfff, 32'hfff, 32'hffe001     );
+    init( 3, 32'ha882, 32'he21, 32'h94cd4c2   );
+    init( 4, 32'he8cd, 32'hd190, 32'hbe925050 );
+    init( 5, 32'h5fc2, 32'h9c3b, 32'h3a7049b6 );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: large positive * negative
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 6, "large positive * negative" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'hab, -32'hbc, -32'h7d94         );
+    init( 1, 32'h2c, -32'h88, -32'h1760         );
+    init( 2, 32'hfff, -32'hfff, -32'hffe001     );
+    init( 3, 32'ha882, -32'he21, -32'h94cd4c2   );
+    init( 4, 32'he8cd, -32'hd190, -32'hbe925050 );
+    init( 5, 32'h5fc2, -32'h9c3b, -32'h3a7049b6 );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: large negative * positive
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 7, "large negative * positive" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, -32'hab, 32'hbc, -32'h7d94         );
+    init( 1, -32'h2c, 32'h88, -32'h1760         );
+    init( 2, -32'hfff, 32'hfff, -32'hffe001     );
+    init( 3, -32'ha882, 32'he21, -32'h94cd4c2   );
+    init( 4, -32'he8cd, 32'hd190, -32'hbe925050 );
+    init( 5, -32'h5fc2, 32'h9c3b, -32'h3a7049b6 );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: large negative * negative
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 8, "large negative * negative" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, -32'hab, -32'hbc, 32'h7d94         );
+    init( 1, -32'h2c, -32'h88, 32'h1760         );
+    init( 2, -32'hfff, -32'hfff, 32'hffe001     );
+    init( 3, -32'ha882, -32'he21, 32'h94cd4c2   );
+    init( 4, -32'he8cd, -32'hd190, 32'hbe925050 );
+    init( 5, -32'h5fc2, -32'h9c3b, 32'h3a7049b6 );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+/*
+  //----------------------------------------------------------------------
+  // Test Case: products with low order bits masked off
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 9, "products with low order bits masked off" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'hax, 32'hbx, 32'h7d94           );
+    init( 1, 32'hax, 32'hbx, 32'h7d94           );
+    init( 3, -32'ha88x, -32'he2x, 32'h94cd4c2   );
+    init( 2, -32'hffx, 32'hffx, 32'hffe001      );
+    init( 4, 32'he8cx, 32'hd19x, 32'hbe925050   );
+    init( 5, 32'h5fcx, -32'h9c3x, 32'h3a7049b6  );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: products with middle bits masked off
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 10, "products with middle bits masked off" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'hxa, 32'hbx, 32'h7d94          );
+    init( 1, -32'h2x, 32'h8x, 32'h1760         );
+    init( 2, -32'hffx, -32'hffx, 32'hffe001    );
+    init( 3, 32'ha88x, 32'he2x, 32'h94cd4c2    );
+    init( 4, 32'he8cx, -32'hd19x, 32'hbe925050 );
+    run_test;
+  end
+  `VC_TEST_CASE_END */
+
+
+  //----------------------------------------------------------------------
+  // Test Case: sparse number * sparse number
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 9, "sparse number * sparse number" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'h82, 32'h01, 32'h82             );
+    init( 1, -32'h20, 32'd08, -32'h100          );
+    init( 2, -32'h808, -32'h4201, 32'h2121808   );
+    init( 3, 32'h1001, 32'h0101, 32'h101101     );
+    init( 4, 32'h4020, -32'h0110, -32'h442200    );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
+
+  //----------------------------------------------------------------------
+  // Test Case: dense number * dense number
+  //----------------------------------------------------------------------
+
+  `VC_TEST_CASE_BEGIN( 10, "dense number * dense number" )
+  begin
+    init_rand_delays( 0, 0 );
+    init( 0, 32'hf7, 32'hbf, 32'hb849           );
+    init( 1, -32'hcc, -32'h7d, 32'h639c         );
+    init( 2, 32'hfff, -32'hfff, -32'hffe001     );
+    init( 3, -32'hb7de, 32'heddb, -32'haad5d0ea );
+    init( 4, -32'hcdfe, -32'hfeed, 32'hcd20b826 );
+    run_test;
+  end
+  `VC_TEST_CASE_END
+
   `VC_TEST_SUITE_END
+
 endmodule
 
