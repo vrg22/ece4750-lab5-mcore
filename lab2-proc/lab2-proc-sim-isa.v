c88744b1e2e9205ccfe653be54941e8334c96894
clt67 <clt67@cornell.edu>
2014-09-13 07:46:47 -0400

    Updating from 0.0-311-gce5dec3-dirty

---
+++ lab2-proc/lab2-proc-sim-isa.v
@@ -0,0 +1,8 @@
+//========================================================================
+// lab2-proc-ISASimulator Unit Tests
+//========================================================================
+
+`define LAB2_PROC_IMPL      lab2_proc_ISASimulator
+
+`include "lab2-proc-ISASimulator.v"
+`include "lab2-proc-isa-sim-harness.v"
