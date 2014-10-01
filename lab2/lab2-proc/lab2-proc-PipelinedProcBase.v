33af615fbd1b29e4beaaade5deff67f68dc3c1d2
Akshay Dongaonkar <akd54@cornell.edu>
2014-10-01 14:38:03 -0400

    fixed?

--- lab2/lab2-proc/lab2-proc-PipelinedProcBase.v
+++ lab2/lab2-proc/lab2-proc-PipelinedProcBase.v
@@ -65,11 +65,11 @@ module lab2_proc_PipelinedProcBase
   // data mem req/resp
   //----------------------------------------------------------------------
 
-  logic [31:0]                                        dmemreq_msg_addr;
-  logic [31:0]                                        dmemreq_msg_data;
-  logic [31:0]                                        dmemresp_msg_data;
-  logic [`VC_MEM_REQ_MSG_TYPE_NBITS(8,32,32)-1:0]     dmemreq_type,
-  logic [31:0]                                        imemreq_msg_addr;
+  logic [31:0]    dmemreq_msg_addr;
+  logic [31:0]    dmemreq_msg_data;
+  logic [31:0]    dmemresp_msg_data;
+  logic [ 2:0]    dmemreq_type,
+  logic [31:0]    imemreq_msg_addr;
 
   // imereq_enq signals coming in from the ctrl unit
   logic [`VC_MEM_REQ_MSG_NBITS(8,32,32)-1:0] imemreq_enq_msg;
