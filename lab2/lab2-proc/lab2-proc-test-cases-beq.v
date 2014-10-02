87ebc709a8929e49b04537f92ca8b07fe191cd16
vrg22 <vrg22@cornell.edu>
2014-10-01 21:58:18 -0400

    BEQ and BLTZ tests written and work on base pipelined processor

--- lab2/lab2-proc/lab2-proc-test-cases-beq.v
+++ lab2/lab2-proc/lab2-proc-test-cases-beq.v
@@ -69,7 +69,558 @@ begin
 end
 endtask
 
-// add more test vectors here
+//------------------------------------------------------------------------
+// Bypassing tests
+//------------------------------------------------------------------------
+
+task init_beq_bypass;
+begin
+
+  clear_mem;
+
+  address( c_reset_vector );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 0 ), src1_nops( 0 )
+  // - src0 loaded before src1
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd1 );
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 0 ), src1_nops( 1 )
+  // - src0 loaded before src1
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd1 );
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 0 ), src1_nops( 2 )
+  // - src0 loaded before src1
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd1 );
+  inst( "nop               ");
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 1 ), src1_nops( 0 )
+  // - src0 loaded before src1
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd1 );
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 1 ), src1_nops( 1 )
+  // - src0 loaded before src1
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd1 );
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 2 ), src1_nops( 0 )
+  // - src0 loaded before src1
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "nop               ");
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd1 );
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 0 ), src1_nops( 0 )
+  // - src1 loaded before src0
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd0 );
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd1 );
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 0 ), src1_nops( 1 )
+  // - src1 loaded before src0
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd1 );
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 0 ), src1_nops( 2 )
+  // - src1 loaded before src0
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "nop               ");
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd1 );
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 1 ), src1_nops( 0 )
+  // - src1 loaded before src0
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd0 );
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd1 );
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 1 ), src1_nops( 1 )
+  // - src1 loaded before src0
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd1 );
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing beq:
+  // src0_nops( 2 ), src1_nops( 0 )
+  // - src1 loaded before src0
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r2, mngr2proc"); init_src( 32'd0 );
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd1 );
+  inst( "nop               ");
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "beq r1, r2, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+
+end
+endtask
+
+//------------------------------------------------------------------------
+// Value tests
+//------------------------------------------------------------------------
+
+task init_beq_value;
+begin
+
+  clear_mem;
+
+  address( c_reset_vector );
+
+  //----------------------------------------------------------------------
+  // Branch tests
+  //----------------------------------------------------------------------
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( 0 ), src1( 0 )
+  // - Test that branch is taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 0 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src( 0 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src( 0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b110 );
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( 1 ), src1( 1 )
+  // - Test that branch is taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 1 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src( 1 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src( 0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b110 );
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( -1 ), src1( -1 )
+  // - Test that branch is taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( -1 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src( -1 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src(  0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b110 );
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( -2 ), src1( -2 )
+  // - Test that branch is taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( -2 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src( -2 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src(  0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b110 );
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( 0 ), src1( 1 )
+  // - Test that branch is NOT taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 0 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src( 1 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src( 0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b011 );
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( 1 ), src1( 0 )
+  // - Test that branch is NOT taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 1 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src( 0 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src( 0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b011 );
+
+  //-------------------------------------------
+  // (Value) Testing beq:
+  // src0( -1 ), src1( 0 )
+  // - Test that branch is NOT taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( -1 ); // src0
+    inst( "mfc0 r2, mngr2proc"); init_src(  0 ); // src1
+    inst( "mfc0 r3, mngr2proc"); init_src(  0 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "beq r1, r2, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "beq r3, r0, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "beq r1, r2, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b011 );
+
+  //----------------------------------------------------------------------
+  // Test that there is no branch delay slot
+  //----------------------------------------------------------------------
+
+  inst( "mfc0 r1, mngr2proc  " ); init_src( 32'd1 );
+  inst( "addu r2, r0, r0     " );
+  inst( "beq  r0, r0, [+5]   " ); // br -.
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); // < - '
+  inst( "addu r2, r2, r1     " );
+  inst( "mtc0 r2, proc2mngr  " ); init_sink( 32'd2 );
+
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+
+end
+endtask
+
+//------------------------------------------------------------------------
+// Long test
+//------------------------------------------------------------------------
+
+integer idx;
+task init_beq_long;
+begin
+
+  clear_mem;
+
+  address( c_reset_vector );
+
+  //----------------------------------------------------------------------
+  // Test backwards walk (back to back branch taken)
+  //----------------------------------------------------------------------
+
+  inst( "mfc0  r1, mngr2proc  "); init_src( 32'd0 );
+  for ( idx = 0; idx < 10; idx = idx + 1 ) begin
+    inst( "beq   r1, r0, [+13]  ");
+    inst( "mtc0  r0, proc2mngr  "); // we don't expect a message here
+    inst( "nop                  ");
+    inst( "mtc0  r1, proc2mngr  "); init_sink(32'd0 );
+    inst( "beq   r1, r0, [+10]  ");
+    inst( "beq   r1, r0, [-2]   "); // goto two above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+    inst( "beq   r1, r0, [-1]   "); // goto one above
+  end
+
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+
+end
+endtask
 
 //------------------------------------------------------------------------
 // Test Case: beq basic
@@ -83,6 +634,40 @@ begin
 end
 `VC_TEST_CASE_END
 
-// add more test cases here
+//------------------------------------------------------------------------
+// Test Case: beq bypass
+//------------------------------------------------------------------------
+
+`VC_TEST_CASE_BEGIN( 2, "beq bypass" )
+begin
+  init_rand_delays( 0, 0, 0 );
+  init_beq_bypass;
+  run_test;
+end
+`VC_TEST_CASE_END
+
+//------------------------------------------------------------------------
+// Test Case: beq value
+//------------------------------------------------------------------------
+
+`VC_TEST_CASE_BEGIN( 3, "beq value" )
+begin
+  init_rand_delays( 0, 0, 0 );
+  init_beq_value;
+  run_test;
+end
+`VC_TEST_CASE_END
+
+//------------------------------------------------------------------------
+// Test Case: beq stalls/bubbles
+//------------------------------------------------------------------------
+
+`VC_TEST_CASE_BEGIN( 4, "beq stalls/bubbles" )
+begin
+  init_rand_delays( 4, 4, 4 );
+  init_beq_long;
+  run_test;
+end
+`VC_TEST_CASE_END
 
 
--- lab2/lab2-proc/lab2-proc-test-cases-bltz.v
+++ lab2/lab2-proc/lab2-proc-test-cases-bltz.v
@@ -68,7 +68,299 @@ begin
 end
 endtask
 
-// add more test vectors here
+//------------------------------------------------------------------------
+// Bypassing tests
+//------------------------------------------------------------------------
+
+task init_bltz_bypass;
+begin
+
+  clear_mem;
+
+  address( c_reset_vector );
+
+  //-------------------------------------------
+  // (Bypass) Testing bltz:
+  // src0_nops( 0 )
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+
+  // forward branch, we assume not taken
+  inst( "bltz r1, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing bltz:
+  // src0_nops( 1 )
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "bltz r1, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+  //-------------------------------------------
+  // (Bypass) Testing bltz:
+  // src0_nops( 2 )
+  //-------------------------------------------
+
+  // load the bitvector
+  inst( "addiu r5, r0, 0   ");
+
+  // load the sources
+  inst( "mfc0 r1, mngr2proc"); init_src( 32'd0 );
+  inst( "nop               ");
+  inst( "nop               ");
+
+  // forward branch, we assume not taken
+  inst( "bltz r1, [+2]" );
+  inst( "addiu r5, r5, 0b1" );
+
+  // check bitvector
+  inst( "mtc0 r5, proc2mngr"); init_sink( 32'b1 );
+
+end
+endtask
+
+//------------------------------------------------------------------------
+// Value tests
+//------------------------------------------------------------------------
+
+task init_bltz_value;
+begin
+
+  clear_mem;
+
+  address( c_reset_vector );
+
+  //----------------------------------------------------------------------
+  // Branch tests
+  //----------------------------------------------------------------------
+
+  //-------------------------------------------
+  // (Value) Testing bltz:
+  // src0( -1 )
+  // - Test that branch is taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( -1 ); // src0
+    inst( "mfc0 r3, mngr2proc"); init_src( -1 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "bltz r1, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "bltz r3, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "bltz r1, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b110 );
+
+  //-------------------------------------------
+  // (Value) Testing bltz:
+  // src0( 0x80000000 )          //largest negative 32-bit value        //CHECK!
+  // - Test that branch is taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( -2147483648 ); // src0          //0x80000000 = -2147483648 in decimal
+    inst( "mfc0 r3, mngr2proc"); init_src( -1 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "bltz r1, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "bltz r3, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "bltz r1, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b110 );
+
+  //-------------------------------------------
+  // (Value) Testing bltz:
+  // src0( 0 )
+  // - Test that branch is NOT taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 0 );  // src0
+    inst( "mfc0 r3, mngr2proc"); init_src( -1 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "bltz r1, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "bltz r3, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "bltz r1, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b011 );
+
+  //-------------------------------------------
+  // (Value) Testing bltz:
+  // src0( 1 )
+  // - Test that branch is NOT taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 1 ); // src0
+    inst( "mfc0 r3, mngr2proc"); init_src( -1 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "bltz r1, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "bltz r3, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "bltz r1, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b011 );
+
+  //-------------------------------------------
+  // (Value) Testing bltz:
+  // src0( 0x7fffffff )                   //Largest Positive Value      //CHECK!
+  // - Test that branch is NOT taken
+  //-------------------------------------------
+
+    // load the bitvector
+    inst( "addiu r5, r0, 0   ");
+
+    // load the sources
+    inst( "mfc0 r1, mngr2proc"); init_src( 2147483647 ); // src0              //0x7fffffff = 2147483647 in decimal
+    inst( "mfc0 r3, mngr2proc"); init_src( -1 ); // helper
+
+    // forward branch, if taken goto 2:
+    inst( "bltz r1, [+4]" );
+    inst( "addiu r5, r5, 0b1" );
+
+    // 1: goto 3:
+    inst( "addiu r5, r5, 0b10" );
+    inst( "bltz r3, [+3]" );
+
+    // 2: backward branch, if taken goto 1:
+    inst( "addiu r5, r5, 0b100" );
+    inst( "bltz r1, [-3]" );
+
+    // 3: check bitvector
+    inst( "mtc0 r5, proc2mngr"); init_sink( 32'b011 );
+
+  //----------------------------------------------------------------------
+  // Test that there is no branch delay slot
+  //----------------------------------------------------------------------
+
+  inst( "mfc0 r1, mngr2proc  " ); init_src( -32'd1 );
+  inst( "addu r2, r0, r0     " );
+  inst( "bltz r1, [+5]       " ); // br -.
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); //     |
+  inst( "addu r2, r2, r1     " ); // < - '
+  inst( "addu r2, r2, r1     " );
+  inst( "mtc0 r2, proc2mngr  " ); init_sink( -32'd2 );
+
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+
+end
+endtask
+
+//------------------------------------------------------------------------
+// Long test
+//------------------------------------------------------------------------
+
+integer idx;
+task init_bltz_long;
+begin
+
+  clear_mem;
+
+  address( c_reset_vector );
+
+  //----------------------------------------------------------------------
+  // Test backwards walk (back to back branch taken)
+  //----------------------------------------------------------------------
+
+  inst( "mfc0  r1, mngr2proc  "); init_src( -32'd1 );
+  for ( idx = 0; idx < 10; idx = idx + 1 ) begin
+    inst( "bltz   r1,     [+13]  ");
+    inst( "mtc0  r0, proc2mngr  "); // we don't expect a message here
+    inst( "nop                  ");
+    inst( "mtc0  r1, proc2mngr  "); init_sink( -32'd1 );
+    inst( "bltz   r1, [+10]  ");
+    inst( "bltz   r1, [-2]   "); // goto two above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+    inst( "bltz   r1, [-1]   "); // goto one above
+  end
+
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+  inst( "nop                  " );
+
+end
+endtask
+
 
 //------------------------------------------------------------------------
 // Test Case: bltz basic
@@ -82,6 +374,42 @@ begin
 end
 `VC_TEST_CASE_END
 
-// add more test cases here
+//------------------------------------------------------------------------
+// Test Case: bltz bypass
+//------------------------------------------------------------------------
+
+`VC_TEST_CASE_BEGIN( 2, "bltz bypass" )
+begin
+  init_rand_delays( 0, 0, 0 );
+  init_bltz_bypass;
+  run_test;
+end
+`VC_TEST_CASE_END
+
+//------------------------------------------------------------------------
+// Test Case: bltz value
+//------------------------------------------------------------------------
+
+`VC_TEST_CASE_BEGIN( 3, "bltz value" )
+begin
+  init_rand_delays( 0, 0, 0 );
+  init_bltz_value;
+  run_test;
+end
+`VC_TEST_CASE_END
+
+//------------------------------------------------------------------------
+// Test Case: bltz stalls/bubbles
+//------------------------------------------------------------------------
+
+`VC_TEST_CASE_BEGIN( 4, "bltz stalls/bubbles" )
+begin
+  init_rand_delays( 4, 4, 4 );
+  init_bltz_long;
+  run_test;
+end
+`VC_TEST_CASE_END
+
+
 
 
