a4758fdeaf855b5dfabb81d5737d72bd71312ad6
akd54 <akd54@cornell.edu>
2014-10-21 17:37:49 -0400

    directory structure set for lab3

--- ex-gcd/ex-gcd-GcdUnit.t.v
+++
@@ -1,9 +0,0 @@
-//=========================================================================
-// ex-gcd-GcdUnit Unit Tests
-//=========================================================================
-
-`define EX_GCD_IMPL ex_gcd_GcdUnit
-
-`include "ex-gcd-GcdUnit.v"
-`include "ex-gcd-test-harness.v"
-
