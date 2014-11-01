9c5bfd189c8d77852d0e4f239664cd0e1283ced6
vrg22 <vrg22@cornell.edu>
2014-10-31 20:24:01 -0400

    Pushed updated random test generator file as well as test-harness with that test

--- lab3/lab3-mem/ubmark-randtest-suite.py
+++ lab3/lab3-mem/ubmark-randtest-suite.py
@@ -24,7 +24,7 @@ def print_cache_read( addr, data ):
 
 def print_cache_write( addr, data ):
   print "init_port( c_req_wr, 8'h00, 32'h{:0>8x}, 2'd0, 32'h{:0>8x}, c_resp_wr, 8'h00, 2'd0, 32'h???????? );" \
-    .formt( addr, data )
+    .format( addr, data )
 
 if len( sys.argv ) < 2:
   print "please provide an argument"
@@ -121,6 +121,49 @@ if sys.argv[1] == "rand_1":
   for j in xrange(100):
     addr = base_addr + j * 4
     print_cache_read( addr, ref_mem[j] )	#Check against rem_mem value
+
+#--------------------------------------------------------------------------------
+# Random test 2: Random address patterns, request types, and data
+#--------------------------------------------------------------------------------
+# Random address pattern here: make random memory reads in a fixed memory range,
+#                              but make a specific access only up to 2 times
+
+if sys.argv[1] == "rand_2":
+
+  ref_mem = {}
+  rep_mem = {}
+  bit = 0
+  temp_data = 0
+  tmp = 0
+  w = [0 for i in range(4)]
+
+  # initialize memory with random data in cache-line address range 0x1000 - 0x1180
+  base_addr = 0x1000
+  for j in xrange(0,100,4):
+    addr = base_addr + j * 4
+    for i in range(4):  #set ref_mem
+      w[i] = random.randint(0, 0xffffffff)
+      ref_mem[j+i] = w[i]
+      rep_mem[j+i] = 0        #Counts of accesses to this addresses
+    print_load_mem( addr, w[0], w[1], w[2], w[3] )
+
+  # generate random memory requests (writes or reads) a total of 100 times
+  for i in xrange(100):
+    #pick a random type of memory request (write or read)
+    bit = random.randint(0, 1)
+    #randomly pick an address by picking a random number in 0-100           #CHECK!!!!
+    tmp = random.randint(1, 100) - 1  #Gives a number from 0-99
+    addr = base_addr + tmp * 4
+    while rep_mem[tmp] == 2:  #Don't refer to the same memory location more than twice
+      tmp = random.randint(1, 100) - 1
+      addr = base_addr + tmp * 4
+    rep_mem[tmp] += 1        #Now that we have an addr, make sure to increment num accesses to it
+    if bit == 0:
+      print_cache_read(  addr, ref_mem[tmp] )  #Check against ref_mem value
+    else:
+      temp_data = random.randint(0, 0xffffffff)
+      ref_mem[tmp] = temp_data    #update ref_mem
+      print_cache_write( addr, temp_data)
 
 #-------------------------------------------------------------------------
 # Unrecognized dataset
