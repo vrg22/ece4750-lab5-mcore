172b54fea7408c96e6b20164d5e90812b84f8e7e
Akshay Dongaonkar <akd54@cornell.edu>
2014-12-01 14:18:53 -0500

    snapshotting a mtbenchmark that works

--- lab5/app/ubmark/mtbmark/mtbmark-sort.c
+++ lab5/app/ubmark/mtbmark/mtbmark-sort.c
@@ -50,7 +50,6 @@ void qsort_in_place( int*src, int first, int last)
 __attribute__ ((noinline))
 void sort_scalar( int* src, int size )
 {
-  printf("Sort size is %d\n",size);
   qsort_in_place(src,0,size-1);
 }
 
@@ -95,7 +94,7 @@ int main( int argc, char* argv[] )
   {
     dest[i] = 0;
   }
-  int max_int = 0x8FFFFFFF;
+//  int min_int = 0x90000000;
 
   int* offset_zero   = src;
   int* offset_one    = src + core_size;
@@ -126,10 +125,6 @@ int main( int argc, char* argv[] )
 #endif
   join();
 
-  for( i = 0; i < core_size; i++)
-  {
-    printf("0: %d, 1: %d, 2: %d, 3: %d\n",*(offset_zero+i),*(offset_one+i),*(offset_two+i),*(offset_three+i));
-  }
 
   if( core_id == 0 )
   {
@@ -141,11 +136,8 @@ int main( int argc, char* argv[] )
           count_zero < core_size)
       {
         dest[i] = *offset_zero;
-        printf("Inserted %d from 0\n",dest[i]);
         offset_zero++;
         count_zero++;
-        printf("Count 0 is %d\n",count_zero);
-        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
       else if( (*offset_one <= *offset_zero  || count_zero  == core_size) && 
                (*offset_one <= *offset_two   || count_two   == core_size) &&
@@ -153,11 +145,8 @@ int main( int argc, char* argv[] )
                count_one < core_size )
       {
         dest[i] = *offset_one;
-        printf("Inserted %d from 1\n",dest[i]);
         offset_one++;
         count_one++;
-        printf("Count 1 is %d\n",count_one);
-        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
       else if( (*offset_two <= *offset_zero  || count_zero  == core_size) && 
                (*offset_two <= *offset_one   || count_one   == core_size) &&
@@ -165,20 +154,14 @@ int main( int argc, char* argv[] )
                count_two < core_size )
       {
         dest[i] = *offset_two;
-        printf("Inserted %d from 2\n",dest[i]);
         offset_two++;
         count_two++;
-        printf("Count 2 is %d\n",count_two);
-        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
       else
       {
         dest[i] = *offset_three;
-        printf("Inserted %d from 3\n",dest[i]);
         offset_three++;
         count_three++;
-        printf("Count 3 is %d\n",count_three);
-        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
     }
 
