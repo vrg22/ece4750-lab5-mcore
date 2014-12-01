48efe9a502ad7c913fa66d37ba924b2955d7afd7
Akshay Dongaonkar <akd54@cornell.edu>
2014-12-01 14:09:46 -0500

    multisort done

--- lab5/app/ubmark/mtbmark/mtbmark-sort.c
+++ lab5/app/ubmark/mtbmark/mtbmark-sort.c
@@ -50,9 +50,7 @@ void qsort_in_place( int*src, int first, int last)
 __attribute__ ((noinline))
 void sort_scalar( int* src, int size )
 {
-  int i;
-  printf("Size is : %d\n",size);  
-
+  printf("Sort size is %d\n",size);
   qsort_in_place(src,0,size-1);
 }
 
@@ -81,6 +79,8 @@ int main( int argc, char* argv[] )
 
   int num_cores = get_num_cores();
 
+  printf("Number of cores is %d\n",num_cores);
+
   // Size of the part of the vector each core will compute
 
   int core_size = get_core_size( size );
@@ -95,11 +95,17 @@ int main( int argc, char* argv[] )
   {
     dest[i] = 0;
   }
+  int max_int = 0x8FFFFFFF;
 
   int* offset_zero   = src;
   int* offset_one    = src + core_size;
-  int* offest_two    = src + 2*core_size;
+  int* offset_two    = src + 2*core_size;
   int* offset_three  = src + 3*core_size;
+
+  int count_zero  = 0;
+  int count_one   = 0;
+  int count_two   = 0;
+  int count_three = 0;
 
   //--------------------------------------------------------------------
   // Start counting stats
@@ -110,32 +116,69 @@ int main( int argc, char* argv[] )
   // Spawn threads and perform parallel computation
 
   spawn();
-  sort_scalar(src+core_id*core_size, core_size)
+#ifdef _MIPS_ARCH_MAVEN
+  sort_scalar(src+core_id*core_size, core_size);
+#else
+  sort_scalar(src, core_size);
+  sort_scalar(src+core_size, core_size);
+  sort_scalar(src+core_size+core_size, core_size);
+  sort_scalar(src+core_size+core_size+core_size, core_size);
+#endif
   join();
+
+  for( i = 0; i < core_size; i++)
+  {
+    printf("0: %d, 1: %d, 2: %d, 3: %d\n",*(offset_zero+i),*(offset_one+i),*(offset_two+i),*(offset_three+i));
+  }
 
   if( core_id == 0 )
   {
     for( i = 0 ; i < size ; i++ )
     {
-      if( *offset_zero < *offset_one && *offset_zero < *offest_two && *offset_zero < *offset_three )
+      if( (*offset_zero <= *offset_one   || count_one  == core_size) && 
+          (*offset_zero <= *offset_two   || count_two   == core_size) &&
+          (*offset_zero <= *offset_three || count_three == core_size) &&
+          count_zero < core_size)
       {
         dest[i] = *offset_zero;
+        printf("Inserted %d from 0\n",dest[i]);
         offset_zero++;
+        count_zero++;
+        printf("Count 0 is %d\n",count_zero);
+        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
-      else if( *offset_one < *offset_zero && *offset_one < *offest_two && *offset_one < *offset_three )
+      else if( (*offset_one <= *offset_zero  || count_zero  == core_size) && 
+               (*offset_one <= *offset_two   || count_two   == core_size) &&
+               (*offset_one <= *offset_three || count_three == core_size) &&
+               count_one < core_size )
       {
         dest[i] = *offset_one;
+        printf("Inserted %d from 1\n",dest[i]);
         offset_one++;
+        count_one++;
+        printf("Count 1 is %d\n",count_one);
+        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
-      else if( *offset_two < *offset_zero && *offset_two < *offest_one && *offset_two < *offset_three )
+      else if( (*offset_two <= *offset_zero  || count_zero  == core_size) && 
+               (*offset_two <= *offset_one   || count_one   == core_size) &&
+               (*offset_two <= *offset_three || count_three == core_size) &&
+               count_two < core_size )
       {
         dest[i] = *offset_two;
+        printf("Inserted %d from 2\n",dest[i]);
         offset_two++;
+        count_two++;
+        printf("Count 2 is %d\n",count_two);
+        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
       else
       {
         dest[i] = *offset_three;
+        printf("Inserted %d from 3\n",dest[i]);
         offset_three++;
+        count_three++;
+        printf("Count 3 is %d\n",count_three);
+        printf("Now 0: %d, 1: %d, 2: %d, 3: %d\n\n",*(offset_zero),*(offset_one),*(offset_two),*(offset_three));
       }
     }
 
