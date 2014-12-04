f20eec1a248ef19496c1caf53434d6ec9eccb7d8
abn44 <abn44@cornell.edu>
2014-12-04 01:22:48 -0500

    fixed proc0 simulator bug

--- lab5/sim/lab5-mcore/lab5-mcore-sim-alt.v
+++ lab5/sim/lab5-mcore/lab5-mcore-sim-alt.v
@@ -3,7 +3,7 @@
 //========================================================================
 
 `define LAB5_MCORE_IMPL             lab5_mcore_ProcCacheNetAlt
-`define LAB5_MCORE_PROC0            proc0
+`define LAB5_MCORE_PROC0            CORES[0].PROC.proc0
 `define LAB5_MCORE_IMPL_STR         "lab5-mcore-ProcCacheNetAlt"
 
 `include "lab5-mcore-ProcCacheNetAlt.v"
